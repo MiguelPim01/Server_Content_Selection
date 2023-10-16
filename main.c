97665041413289bd6f2fda7745180a09a3f47c8e
Murilo <murilo.salvador95@gmail.com>
2023-10-15 23:34:19 -0300

    Fazendo com listas

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "src/ed/graph.h"
+#include "headers/graph.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,3 +14,12 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+/* Vector : 10,656 allocs, 10,656 frees, 2,096,296 bytes */
+
+/* List :
+    10000 : 39,999 allocs, 39,999 frees, 1,058,968 bytes | 0m0.176s
+    1000  : 3,999 allocs,  3,999 frees,  113,968 bytes   | 0m0.018s
+    100   : 399 allocs,    399 frees,    19,468 bytes    | 0m0.004s
+    10    : 39 allocs,     39 frees,     10,018 bytes    | 0m0.003s
+*/ 
